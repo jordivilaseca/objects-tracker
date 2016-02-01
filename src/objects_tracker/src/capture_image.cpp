cba4d395009cb0a7783c4992e19eaa0e613069d3
Jordi Vilaseca <jordi.vilaseca.corderroure@est.fib.upc.edu>
2016-02-01 11:58:23 +0100

    Using only one callback

--- src/objects_tracker/src/capture_image.cpp
+++ src/objects_tracker/src/capture_image.cpp
@@ -4,6 +4,12 @@
 #include "ros/ros.h"
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
+#include <message_filters/subscriber.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
 
 // opencv inclues
 #include <opencv2/highgui/highgui.hpp>
@@ -11,10 +17,12 @@
 using namespace cv;
 using namespace std;
 using namespace cv_bridge;
+using namespace sensor_msgs;
+using namespace message_filters;
 
 bool newFrame1 = false;
 bool newFrame2 = false;
-cv::Mat colorFrame, depthFrame;
+cv::Mat colorFrame, depthFrame, dst;
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
@@ -52,6 +60,38 @@ void depthCallback(const sensor_msgs::ImageConstPtr& msg)
     }
   }
 
+void callback(const sensor_msgs::ImageConstPtr& color, const sensor_msgs::ImageConstPtr& depth, const sensor_msgs::CameraInfoConstPtr& info) {
+  
+  // Process color frame.
+  colorFrame = cv_bridge::toCvCopy(color, "bgr8")->image;
+  cv::imshow("color_view_cam_1", colorFrame);
+  cv::waitKey(2);
+
+  // Process depth frame.
+  depthFrame = (cv_bridge::toCvCopy(depth, "16UC1")->image);
+  double min, max;  
+  minMaxIdx(depthFrame, &min, &max);
+  depthFrame.convertTo(depthFrame,CV_32F, 1.0/max);
+  newFrame2 = true;
+
+  // Show frame.
+  cv::imshow("depth_view_cam_1", depthFrame);
+  cv::waitKey(2);
+
+  // Process overlapped frames
+  for(uint i = 0; i < depthFrame.rows; i++) {
+    for(uint j = 0; j < depthFrame.cols; j++) {
+      if(depthFrame.at<float>(i, j) == 0) {
+        dst.at<Vec3b>(i,j) = Vec3b(0,0,0);
+      } else {
+        dst.at<Vec3b>(i,j) = colorFrame.at<Vec3b>(i,j);
+      }
+    }
+  }
+  cv::imshow("combination_view", dst);
+  cv::waitKey(2);
+}
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "capture_image");
@@ -59,11 +99,23 @@ int main(int argc, char **argv)
   cv::namedWindow("color_view_cam_1", WINDOW_NORMAL);
   cv::namedWindow("depth_view_cam_1", WINDOW_NORMAL);
   cv::namedWindow("combination_view", WINDOW_NORMAL);
-  cv::startWindowThread();  
+  cv::startWindowThread();
+
+  message_filters::Subscriber<Image> color1_sub(nh,"/cam1/qhd/image_color_rect", 1);
+  message_filters::Subscriber<Image> depth1_sub(nh,"/cam1/qhd/image_depth_rect", 1);
+  message_filters::Subscriber<CameraInfo> info_sub(nh,"/camera/rgb/camera_info", 1);
+
+  typedef sync_policies::ApproximateTime<Image, Image, CameraInfo, CameraInfo> MySyncPolicy;
+  Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), color1_sub, depth1_sub, info_sub);
+
+  sync.registerCallback(boost::bind(&callback, _1, _2, _3));
+
+  ros::spin();
   
-  image_transport::ImageTransport it(nh);
-  image_transport::Subscriber color_sub_cam_1 = it.subscribe("/cam1/qhd/image_color_rect", 1, imageCallback);
-  image_transport::Subscriber depth_sub_cam_1 = it.subscribe("/cam1/qhd/image_depth_rect", 1, depthCallback);
+  /*image_transport::ImageTransport it(nh);
+  image_transport::Subscriber color1_sub = it.subscribe("/cam1/qhd/image_color_rect", 1, imageCallback);
+  image_transport::Subscriber depth1_sub = it.subscribe("/cam1/qhd/image_depth_rect", 1, depthCallback);
+  message_filters::Subscriber info_cam_1 = nh.subscribe("/cam1/qhd/camera_info", 1, infoCallback);
 
   bool first = true;
   Mat dst;
@@ -92,7 +144,7 @@ int main(int argc, char **argv)
       cv::waitKey(2);
     }
     ros::spinOnce();
-  }
+  }*/
   cv::destroyWindow("color_view_cam_1");
   cv::destroyWindow("depth_view_cam_1");
   cv::destroyWindow("combination_view");
