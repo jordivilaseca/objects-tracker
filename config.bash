24facc9bbb24da47b273f8b5982cd8f400776326
Jordi Vilaseca <jordi.vilaseca.corderroure@est.fib.upc.edu>
2016-02-23 15:16:17 +0100

    Updated config.bash

--- config.bash
+++ config.bash
@@ -4,11 +4,15 @@
 source /opt/ros/indigo/setup.bash
 export ROS_IP=192.168.2.10
 
+# For SSH connection to server
+export ROSLAUNCH_SSH_UNKNOWN=1
+
 #### Set specific ros configuration ####
 source ~/catkin_ws/devel/setup.bash
 
 #### Define some util alias #####
 alias inhands="ssh ros@192.168.2.245"
+alias shipit="~/catkin_ws/shipit.bash"
 
 # Use roscore from the server
 alias confmaster="ROS_MASTER_URI=http://192.168.2.245:11311/"
