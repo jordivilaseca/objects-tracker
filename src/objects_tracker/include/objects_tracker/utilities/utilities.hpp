4b178224b30c859fff5ccd07e3f5b561339bed58
Jordi Vilaseca <jordi.vilaseca.corderroure@est.fib.upc.edu>
2016-04-05 12:04:24 +0200

    Change color calculation function

--- src/objects_tracker/include/objects_tracker/utilities/utilities.hpp
+++ src/objects_tracker/include/objects_tracker/utilities/utilities.hpp
@@ -1,3 +1,5 @@
 #include <vector>
+#include <cmath>
 
+void computeColor(int i, int n, std::vector<int> &color);
 void computeColors(int n, std::vector< std::vector<int> > &colours);
--- src/objects_tracker/src/utilities/utilities.cpp
+++ src/objects_tracker/src/utilities/utilities.cpp
@@ -1,42 +1,30 @@
 #include <objects_tracker/utilities/utilities.hpp>
 
+void computeColor(int i, int n, std::vector<int> &color) {
+	double param, fractpart, intpart;
+	param = ((float) i)/((float) n)*4.0;
+	fractpart = std::modf(param , &intpart);
+	switch ((int) intpart) {
+		case 0:
+			color = {255, (int) (255*fractpart), 0};
+			break;
+		case 1:
+			color = {255 - ((int) (255*fractpart)), 255, 0};
+			break;
+		case 2:
+			color = {0, 255, (int) (255*fractpart)};
+			break;
+		case 3:
+			color = {0, 255 - ((int) (255*fractpart)), 255};
+		default:
+			color = {255, 255, 255};
+	}
+}
+
 void computeColors(int n, std::vector< std::vector<int> > &colours) {
 	colours = std::vector< std::vector<int> >(n, std::vector<int>(3));
 
-	int step = (256*4)/n;
-	int r, g, b;
-	int i = 0;
-
-	// Red and blue are fixed, incrementing green from 0 to 255.
-	r = 255;
-	g = 0;
-	b = 0;
-	for(; g <= 255 and i < n; g += step) {
-		colours[i][0] = r; colours[i][1] = g; colours[i][2] = b;
-		i++;
-	}
-
-	// Green and blue are fixed, decrementing red from 255 to 0.
-	r = 255 - (g - 256);
-	g = 255;
-	for(; r >= 0 and i < n; r -= step) {
-		colours[i][0] = r; colours[i][1] = g; colours[i][2] = b;
-		i++;
-	}
-
-	// Red and green fixed, incrementing blue from 0 to 255.
-	b = -r;
-	r = 0;
-	for(; b <= 255 and i < n; b += step) {
-		colours[i][0] = r; colours[i][1] = g; colours[i][2] = b;
-		i++;
-	}
-
-	// Red and blue fixed, decrementing green from 255 to 0.
-	g = 255 - (b - 256);
-	b = 255;
-	for(; g >= 0 and i < n; g -= step) {
-		colours[i][0] = r; colours[i][1] = g; colours[i][2] = b;
-		i++;
+	for(int i = 0; i < n; i++) {
+		computeColor(i, n, colours[i]);
 	}
 }
